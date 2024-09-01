860ccb49b29700349cba56009f3e7b755570676c
uwi0 <kakapoweb@gmail.com>
2024-09-02 03:47:25 +0700

    Initial Commit

---
+++ Shuzi Game/Shuzi_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shuzi_GameApp.swift
+//  Shuzi Game
+//
+//  Created by dwi prasetyo on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Shuzi_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
