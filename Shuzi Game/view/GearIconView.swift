082e1a7b0705798fca4bd6363e6f4a725af4b34b
uwi0 <kakapoweb@gmail.com>
2024-09-04 03:55:20 +0700

    creating GearIconView

---
+++ Shuzi Game/view/GearIconView.swift
@@ -0,0 +1,18 @@
+//
+//  GearIconView.swift
+//  Shuzi Game
+//
+//  Created by dwi prasetyo on 04/09/24.
+//
+
+import SwiftUI
+
+struct GearIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GearIconView()
+}
